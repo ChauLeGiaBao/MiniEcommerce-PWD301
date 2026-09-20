47832e4f30b78258c8f690f48e15e610a37d4e64
Hà Trung Kiên <hatrungkien2657@gmail.com>
2026-09-20 15:42:47 +0700

    Import all models in __init__

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.danh_muc import DanhMuc
+from app.models.san_pham import SanPham
+from app.models.nguoi_dung import User
+from app.models.don_hang import DonHang
+from app.models.chi_tiet_don_hang import ChiTietDonHang
