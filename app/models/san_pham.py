def8ce1560000691bfd30c12e122621f5789f4b0
Hà Trung Kiên <hatrungkien2657@gmail.com>
2026-09-20 15:39:59 +0700

    Add SanPham model

---
+++ app/models/san_pham.py
@@ -0,0 +1,14 @@
+from app.extensions import db
+
+
+class SanPham(db.Model):
+    __tablename__ = 'SANPHAM'
+    MASANPHAM = db.Column(db.Integer, primary_key=True)
+    TENSPH = db.Column(db.Unicode(200))
+    HINHANH = db.Column(db.String(100))
+    DONGIA = db.Column(db.Numeric(18, 2))
+    MOTA = db.Column(db.Unicode(500))
+    SOLUONGTON = db.Column(db.Integer, default=0)
+    TRANGTHAISPH = db.Column(db.Integer, default=1)
+    MADANHMUC = db.Column(db.Integer, db.ForeignKey('DANHMUC.MADANHMUC'))
+    danhmuc = db.relationship('DanhMuc', backref='sanpham_list')
